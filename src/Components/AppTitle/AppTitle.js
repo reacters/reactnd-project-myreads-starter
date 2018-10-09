4e1956de757d61b7cc209b478ec4ea59babc19f5
strider99 <panigrahiomprakash@gmail.com>
2018-10-09 19:11:10 +0530

    convert app title and move button to components

---
+++ src/Components/AppTitle/AppTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const appTitle = (props) => {
+  return(
+    <div className="list-books-title">
+      <h1>{props.title}</h1>
+    </div>
+  )
+}
+
+export default appTitle;
